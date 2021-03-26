b8e0376da22c1997e6aeeb82507ee2a39b30ab36
Otavio Fevereiro <otavio.fevereiro@hbsins.com.br>
2021-03-26 17:39:24 -0300

    Convert to .net 5

--- DevPack/Extensions/EnumerableExtensions.cs
+++ DevPack/Extensions/EnumerableExtensions.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 
-namespace DevPack.Extensions
+namespace System.Collections.Generic
 {
     public static class EnumerableExtensions
     {
